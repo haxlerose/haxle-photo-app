a44251c93768244ab40d34b2ee41e9e06725f4d4
haxlerose <chestertonpa@gmail.com>
2018-03-13 15:29:39 -0400

    added payment model for credit card transactions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end
